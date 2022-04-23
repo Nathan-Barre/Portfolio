4208a69eb687b8b8987a5046de643bc96999bfed
Nathan <nathan.barre@epitech.eu>
2022-04-23 18:41:20 +0200

    udp: home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
+import "./Home.scss"
+
+function goToPortfolio(language, props) {
+    if (language === 'uk') {
+        console.log("This is shakespeare")
+    } else if (language === 'fr') {
+        console.log("C'est Moliere")
+    }
+}
 
 const Home = () => {
+    const [ukSide, setUkSide] = useState(false);
+    const [frSide, setFrSide] = useState(false);
+
     return (
-        <div>
-            Hello Home
+        <div className='Home'>
+            <div
+                className="English"
+                align='left'
+                onMouseOver={(e) => setFrSide(true)}
+                onMouseOut={(e) => setFrSide(false)}
+                hidden={ukSide}>
+                Welcome to my resume in English
+                <div>
+                    <button
+                        className='HomeButton'
+                        onClick={(e) => {
+                            goToPortfolio('uk')
+                        }}>
+                        Continue
+                    </button>
+                </div>
+            </div>
+            <div
+                className="French"
+                align='right'
+                onMouseOver={(e) => setUkSide(true)}
+                onMouseOut={(e) => setUkSide(false)}
+                hidden={frSide}>
+                Bienvenue sur mon CV en Français
+                <div>
+                    <button className='HomeButton'
+                            onClick={(e) => {
+                                goToPortfolio('fr', frSide)
+                            }}>
+                        Continue
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
